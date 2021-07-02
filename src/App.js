67fc856e47bc083da65bb4e13718bfc47ceff313
kelseyjdickson <kelseymedved@gmail.com>
2021-07-02 10:56:13 -0400

    mapping through image

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Link, Route, Switch } from "react-router-dom";
+
+//componenets
+import Part1 from "./Part1";
+import Part2 from "./Part2"
 //assets
 import part1 from "./images/womenintech.jpeg";
 import part2 from "./images/yellowwit.jpeg";
 import part3 from "./images/part3.jpeg";
 
+const blogData = [
+  {
+    id: 0,
+    link: "https://kelseyjdickson.medium.com/5-women-in-tech-to-watch-in-2021-1df735a9ecbd",
+    image: part1,
+    name: part1
+
+  },
+  {
+    id: 1,
+    link: "https://kelseyjdickson.medium.com/women-to-watch-in-tech-in-2021-9cb2d69543c7",
+    image: part2,
+    name: part2
+
+  },
+  {
+    id: 2,
+    link: "https://kelseyjdickson.medium.com/women-to-watch-in-tech-in-2021-cdb437200fe1",
+    image: part3,
+    name: part2
+
+  },
+
+]
+
 function App() {
+
+  let image = blogData.map((bl) => <img src={bl.image} />)
   return (
     <div className="App">
-      <h1>Header</h1>
       <div>
-        <img
-          style={{ width: "600px", height: "417px" }}
-          src={part1}
-          alt="first blog"
-        />
-        <img
-          style={{ width: "600px", height: "417px" }}
-          src={part2}
-          alt="second blog"
-        />
-        <img
-          style={{ width: "600px", height: "417px" }}
-          src={part3}
-          alt="third blog"
-        />
+     {image}
       </div>
     </div>
   );
---
+++ src/Part2.js
@@ -0,0 +1,20 @@
+import  React from "react";
+
+//assets
+import part2 from "./images/yellowwit.jpeg";
+
+const Part2 = () => {
+    return(
+        <>
+        <img
+          style={{ width: "600px", height: "417px" }}
+          src={part2}
+          alt="first blog"
+          
+        />
+            <h1>Made it to Part 2</h1>
+        </>
+    )
+}
+
+export default Part2
