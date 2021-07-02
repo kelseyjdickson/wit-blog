eab602fb0cd4c93e9548887a40635e2f5c3cacc8
kelseyjdickson <kelseymedved@gmail.com>
2021-07-02 11:21:16 -0400

    working link to blog

--- src/App.js
+++ src/App.js
@@ -15,33 +15,46 @@ const blogData = [
     id: 0,
     link: "https://kelseyjdickson.medium.com/5-women-in-tech-to-watch-in-2021-1df735a9ecbd",
     image: part1,
-    name: part1
+    name: "Women in Tech to Watch Part I",
+    component: <Part1/>
 
   },
   {
     id: 1,
     link: "https://kelseyjdickson.medium.com/women-to-watch-in-tech-in-2021-9cb2d69543c7",
     image: part2,
-    name: part2
+    name: "Women in Tech to Watch Part II",
 
   },
   {
     id: 2,
     link: "https://kelseyjdickson.medium.com/women-to-watch-in-tech-in-2021-cdb437200fe1",
     image: part3,
-    name: part2
+    name: "Women in Tech to Watch Part III",
 
   },
 
 ]
 
 function App() {
-
-  let image = blogData.map((bl) => <img src={bl.image} />)
+  let image = blogData.map((bl) => {
+  return(
+      
+    <div key={bl.id} style={{display:'flex'}}>
+      <a href={bl.link} style={{display:'flex'}}>
+      <img src={bl.image} style={{width: '300px', display:'flex',}}/>
+     </a>
+      
+      <h1>{bl.name}</h1>
+      
+  </div>
+  )})
+  
   return (
     <div className="App">
       <div>
      {image}
+    
       </div>
     </div>
   );
