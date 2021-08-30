3129f1c45e34fb6948bd6a75437f0bcb1087a041
kelseyjdickson <kelseymedved@gmail.com>
2021-08-30 14:17:50 -0400

    added linked in button

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -16,11 +16,11 @@ const Home = ({ data }) => {
           <div key={womenInfo.id} className="container">
             <img className="container__image"src={womenInfo.female.image} alt="women in tech" />
             <h1 className="container__header">{womenInfo.female.name}</h1>
-            <p>Company: {womenInfo.female.company}</p>
-            <p>Job: {womenInfo.female.job_title}</p>
-            <p>
-              <a href={womenInfo.female.contact_link}>LinkedIn</a>
-            </p> 
+            <p className="container__company"><b>Company:</b> {womenInfo.female.company}</p>
+            <p className="container__job"><b>Job:</b> {womenInfo.female.job_title}</p>
+            
+              <a href={womenInfo.female.contact_link}><i class="fab fa-linkedin fa-2x"></i></a>
+            
           </div>
         );
       })}
