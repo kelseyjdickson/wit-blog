61c6ad1d1d16337a8b603cf5822a8ac7bf313f4e
kelseyjdickson <kelseymedved@gmail.com>
2021-08-30 19:40:07 -0400

    image and styling

--- src/About.js
+++ src/About.js
@@ -4,7 +4,8 @@ import React from 'react'
 const About = () => {
     return(
         <>
-            <h1>About me</h1>
+            <p>When I first joined Twitter there were many lists going around of Devs to follow. They were exclusively spotlighting males. I decided to spend some free time reaching out to 
+     the inspiring women who are making a positive impact in the tech community. </p>
         </>
     )
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 //assets
 import './_header.scss'
-import wit from '../images/Wit.jpeg'
+import wit from '../images/Wit-1.jpeg'
 
 const Header = () => {
     return(
 
     <div>
-        <img src={wit} alt="logo"/>
+        <img className="logo"src={wit} alt="logo"/>
     </div>
         // <div className="container-header">
         // <h1 className="container-header__header">Women In Tech</h1>
