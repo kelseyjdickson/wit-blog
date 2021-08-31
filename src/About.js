6f9b1356ee1e48ebb59800568458d62d57eb9e61
kelseyjdickson <kelseymedved@gmail.com>
2021-08-31 18:57:37 -0400

    updated about

--- src/About.js
+++ src/About.js
@@ -4,8 +4,9 @@ import React from 'react'
 const About = () => {
     return(
         <>
-            <p>When I first joined Twitter there were many lists going around of Devs to follow. They were exclusively spotlighting males. I decided to spend some free time reaching out to 
-     the inspiring women who are making a positive impact in the tech community. </p>
+            <h1>When I first joined Twitter there were many lists going around of Devs to follow. 
+            They were exclusively spotlighting males. I decided to spend some free time reaching out to 
+            the inspiring women who are making a positive impact in the tech community. </h1>
         </>
     )
 }
