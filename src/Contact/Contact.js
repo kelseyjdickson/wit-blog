59a5dfeb8f143003b2b7b4adc43e8ab9ce1362f3
kelseyjdickson <kelseymedved@gmail.com>
2021-09-07 21:12:15 -0400

    working on contact file

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,9 +1,15 @@
-import react from 'react'
+import react from "react";
 
 const Contact = () => {
-    return(
-        <h1>Hello</h1>
-    )
-}
+  return (
+    <>
+      <h1>Follow me on:</h1>
+      <p>Twitter</p>
+      <p>LinkedIn</p>
+      <p>Medium</p>
+      <p>Email me:</p>
+    </>
+  );
+};
 
 export default Contact;
