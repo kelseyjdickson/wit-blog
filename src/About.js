61d8c6ebbfc32c50b2ca8994bdbc68bbae70e00b
kelseyjdickson <kelseymedved@gmail.com>
2021-09-14 17:36:47 -0400

    footer

--- src/About.js
+++ src/About.js
@@ -1,19 +1,28 @@
 import React ,{useState, useEffect }from "react";
-
-
+import axios from 'axios'
 const About = () => {
   const [count, setCount ] = useState(0)
   const [data, setData ]= useState([])
   
-  useEffect(()=>{
-    
-    fetch("https://randomuser.me/api")
-    .then(res => res.json())
-    .then((res)=>{
-      console.log(res.results)
-       setData(res.results)
+  const fetchData = () => {
+    return axios.get("https://randomuser.me/api")
+    .then(res => {
+      console.log(res)
+      return <h1>{res}</h1>
     })
-  },[])
+    .catch(err => {
+      console.error(err)
+
+    })
+  }
+  // useEffect(()=>{
+  //   fetch("https://randomuser.me/api")
+  //   .then(res => res.json())
+  //   .then((res)=>{
+  //     console.log(res.results)
+  //      setData(res.results)
+  //   })
+  // },[])
   return (
     <>
     <ul>
@@ -32,6 +41,7 @@ const About = () => {
       <button onClick={()=> setCount(count + 1)}>add</button>
       <button onClick={()=> setCount(count - 1)}>Minus</button>
       <br></br>
+      <button onClick={fetchData}>Click to Fetch Data</button>
       
     </>
   );
