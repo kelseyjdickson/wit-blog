33a51a230fc14dfbc9230768384443d19c13794a
kelseyjdickson <kelseymedved@gmail.com>
2021-10-25 13:50:54 -0400

    styles to women-detail page"

--- src/WomenDetail/WomenDetail.js
+++ src/WomenDetail/WomenDetail.js
@@ -1,45 +1,44 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom';
-import './_womenDetail.scss'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./_womenDetail.scss";
 
 const WomenDetail = () => {
+  const [womenDetail, setWomenDetail] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:3000/blogs/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        setWomenDetail(data);
+      })
+      .catch(err => console.log(err));
+  }, [id]);
+  const { female } = womenDetail;
 
-    const [womenDetail, setWomenDetail] = useState([])
-    const {id} = useParams()
-    useEffect(() => {
-        fetch(`http://localhost:3000/blogs/${id}`)
-          .then(res => res.json())
-          .then(data => {
-            setWomenDetail(data);
-          })
-          .catch(err => console.log(err))
-      }, [id]);
-      const { female } = womenDetail;
+  return (
+    <div>
+      {!female ? (
+        "Loading..."
+      ) : (
+        <>
+        <div className="women-container">
+          <img className="women-image"src={female.image} alt="showing" />
+          <h1 className="women-name">{female.name} </h1>
+          <h2>Job Title: {female.job_title}</h2>
+          <h3>Company: {female.company}</h3>
 
-    return(
-        <div>
-           {!female ? "Loading..." :
-           <>
-           
-            <img src={female.image} alt="showing"/>
-            <h1>{female.name} </h1>
-            <h2>Job Title: {female.job_title}</h2>
-            <h3>Company: {female.company}</h3>
-            
-            <div className="women-text">
+          <div className="women-text">
             <p>{female.women_text}</p>
-            </div>
-            <div>
-            <a href={female.website}
-           target="_blank"
-           rel="noopener noreferrer">{female.website}</a>
-            </div>
-            </>
-           } 
-        </div>
-      
-        
-        
-    )
-}
+          </div>
+          <div>
+            <a className="women-link"href={female.website} target="_blank" rel="noopener noreferrer">
+              {female.website}
+            </a>
+          </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 export default WomenDetail;
